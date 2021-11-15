a98e3353d540e86eaaa2513e058980f092e7339b
issac deline <issadeli228@outlook.com>
2021-11-15 09:30:55 -0500

    day 2

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -131,7 +131,7 @@ namespace BrickBreaker
                     rightArrowDown = true;
                     break;
                 default:
-                    break;4
+                    break;
             }
         }
 
